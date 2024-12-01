2ba10dfbe4e9255af1b8c8baecc79cc95f486438
Daniël Groeneweg <daniel.groeneweg03@gmail.com>
2024-12-01 15:47:18 +0100

    added sounds

--- Assets/Scripts/ControlAnimatorParameters.cs
+++ Assets/Scripts/ControlAnimatorParameters.cs
@@ -1,17 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ControlAnimatorParameters : MonoBehaviour
 {
     public Animator animator;
+    public UnityEvent match;
+    private bool oldState;
     public void EnableBool(string parameterName)
     {
+        oldState = animator.GetBool(parameterName);
         animator.SetBool(parameterName, true);
     }
 
     public void DisableBool(string parameterName)
     {
+        oldState = animator.GetBool(parameterName);
         animator.SetBool(parameterName, false);
+    }
+
+    public void CheckBool(string parameterName)
+    {
+        if (oldState != animator.GetBool(parameterName)) match?.Invoke();
     }
 }
