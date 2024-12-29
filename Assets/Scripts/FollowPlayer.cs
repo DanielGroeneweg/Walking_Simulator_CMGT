1313f413a0ec1021fa35623c8530035aa1eb0e1f
Daniël Groeneweg <daniel.groeneweg03@gmail.com>
2024-12-29 14:13:06 +0100

    Bug fixing, audio fixes, quality of life fixes
    Bug fixing:
    - Fixed a bug that caused rain to disappear when the player got high enough on the island
    
    Audio fixes:
    - Fixed audio to be played in 3D rather than 2D
    
    Quality of life fixes:
    - Made the player push boxes with less impact, making it easier to control where the box is going

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -5,12 +5,25 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
+
+    private float lastPlayerPosY;
     void Update()
     {
         Vector3 playerPos = player.transform.position;
+
+        // Set the x and z positions to the player's
         Vector3 pos = transform.position;
         pos.x = playerPos.x;
         pos.z = playerPos.z;
+
+        // Calculate Y changes
+        float yChange = playerPos.y - lastPlayerPosY;
+        pos.y += yChange;
+
+        // Make it follow the player
         transform.position = pos;
+
+        // Save the player's last position for calculations in the next frame
+        lastPlayerPosY = playerPos.y;
     }
 }
